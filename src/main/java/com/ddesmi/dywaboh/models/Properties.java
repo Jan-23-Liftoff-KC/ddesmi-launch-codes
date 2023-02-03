7cb7afc288febd71654fcfc28b51178a33cb7728
Matthew Temple <mattdtemple@gmail.com>
2023-02-02 21:22:52 -0600

    testing

--- src/main/java/com/ddesmi/dywaboh/models/Properties.java
+++ src/main/java/com/ddesmi/dywaboh/models/Properties.java
@@ -3,240 +3,222 @@ package com.ddesmi.dywaboh.models;
 import javax.persistence.*;
 import java.util.List;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.Date;
 
 @Entity
 public class Properties {
 
-package com.ddesmi.dywaboh.models;
+    //PROPERTIES
 
-import javax.persistence.*;
-import java.util.List;
-import javax.validation.Valid;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import java.util.ArrayList;
-import java.util.Date;
+    @Id
+    @GeneratedValue
+    @Column(insertable = false, updatable = false)
+    private int id;
 
-    @Entity
-    public class Properties {
+    @NotBlank
+    private String address;
 
-        //PROPERTIES
+    @NotBlank
+    private String city;
 
-        @Id
-        @GeneratedValue
-        @Column(insertable = false, updatable = false)
-        private int id;
+    @NotBlank
+    private String state;
 
-        @NotBlank
-        private String address;
+    @NotBlank
+    private int zip;
 
-        @NotBlank
-        private String city;
+    @NotBlank
+    private Float price;
 
-        @NotBlank
-        private String state;
+    @NotBlank
+    private Float bathrooms;
 
-        @NotNull
-        private int zip;
+    @NotBlank
+    private Float bedrooms;
 
-        @NotNull
-        private Float price;
+    @NotBlank
+    private Boolean centralHeating;
 
-        @NotNull
-        private Float bathrooms;
+    @NotBlank
+    private Boolean centralCooling;
 
-        @NotNull
-        private Float bedrooms;
+    @NotBlank
+    private Boolean garage;
 
-        @NotNull
-        private Boolean centralHeating;
+    @NotBlank
+    private int squareFootage;
 
-        @NotNull
-        private Boolean centralCooling;
+//    @NotBlank
+//    private Date listingDate;
 
-        @NotNull
-        private Boolean garage;
+    @NotBlank
+    private String status;
 
-        @NotNull
-        private int squareFootage;
+    private String schoolArea;
 
-        @NotNull
-        private Date listingDate;
+    @OneToMany
+    @JoinColumn(name="properties_id")
+    private List<Images> images = new ArrayList<>();
 
-        @NotBlank
-        private String status;
-
-        @NotBlank
-        private String schoolArea;
-
-        @OneToMany(mappedBy = "properties")
-        private List<Images> images = new ArrayList<>();
-
-        @OneToMany(mappedBy = "properties")
-        private List<PriceHistory> prices = new ArrayList<>();
-
-        @ManyToOne
-        private Realtors realtors;
-
-//GETTERS AND SETTERS
-
-        public int getId() {
-            return id;
-        }
-
-        public void setId(int id) {
-            this.id = id;
-        }
-
-        public String getAddress() {
-            return address;
-        }
-
-        public void setAddress(String address) {
-            this.address = address;
-        }
-
-        public String getCity() {
-            return city;
-        }
-
-        public void setCity(String city) {
-            this.city = city;
-        }
-
-        public String getState() {
-            return state;
-        }
-
-        public void setState(String state) {
-            this.state = state;
-        }
-
-        public int getZip() {
-            return zip;
-        }
-
-        public void setZip(int zip) {
-            this.zip = zip;
-        }
-
-        public Float getPrice() {
-            return price;
-        }
-
-        public void setPrice(Float price) {
-            this.price = price;
-        }
-
-        public Float getBathrooms() {
-            return bathrooms;
-        }
-
-        public void setBathrooms(Float bathrooms) {
-            this.bathrooms = bathrooms;
-        }
-
-        public Float getBedrooms() {
-            return bedrooms;
-        }
-
-        public void setBedrooms(Float bedrooms) {
-            this.bedrooms = bedrooms;
-        }
-
-        public Boolean getCentralHeating() {
-            return centralHeating;
-        }
-
-        public void setCentralHeating(Boolean centralHeating) {
-            this.centralHeating = centralHeating;
-        }
-
-        public Boolean getCentralCooling() {
-            return centralCooling;
-        }
-
-        public void setCentralCooling(Boolean centralCooling) {
-            this.centralCooling = centralCooling;
-        }
-
-        public Boolean getGarage() {
-            return garage;
-        }
-
-        public void setGarage(Boolean garage) {
-            this.garage = garage;
-        }
-
-        public int getSquareFootage() {
-            return squareFootage;
-        }
-
-        public void setSquareFootage(int squareFootage) {
-            this.squareFootage = squareFootage;
-        }
-
-        public Date getListingDate() {
-            return listingDate;
-        }
-
-        public void setListingDate(Date listingDate) {
-            this.listingDate = listingDate;
-        }
-
-        public String getStatus() {
-            return status;
-        }
-
-        public void setStatus(String status) {
-            this.status = status;
-        }
-
-        public String getSchoolArea() {
-            return schoolArea;
-        }
-
-        public void setSchoolArea(String schoolArea) {
-            this.schoolArea = schoolArea;
-        }
-
-        public List<Images> getImages() {
-            return images;
-        }
-
-        public void setImages(List<Images> images) {
-            this.images = images;
-        }
-
-        public List<PriceHistory> getPrices() {
-            return prices;
-        }
-
-        public void setPrices(List<PriceHistory> prices) {
-            this.prices = prices;
-        }
-
-        public Realtors getRealtors() {
-            return realtors;
-        }
-
-        public void setRealtors(Realtors realtors) {
-            this.realtors = realtors;
-        }
+    @OneToMany
+    @JoinColumn(name="properties_id")
+    private List<PriceHistory> prices = new ArrayList<>();
 
 
+    //GETTERS AND SETTERS
 
-        //CONSTRUCTORS
-        public Properties() {
-        }
-
-        public Properties(ArrayList<Images> images, ArrayList<PriceHistory> prices) {
-            this.images = images;
-            this.prices = prices;
-        }
-
+    public int getId() {
+        return id;
     }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public Float getBathrooms() {
+        return bathrooms;
+    }
+
+    public void setBathrooms(Float bathrooms) {
+        this.bathrooms = bathrooms;
+    }
+
+    public Float getBedrooms() {
+        return bedrooms;
+    }
+
+    public void setBedrooms(Float bedrooms) {
+        this.bedrooms = bedrooms;
+    }
+
+    public Boolean getCentralHeating() {
+        return centralHeating;
+    }
+
+    public void setCentralHeating(Boolean centralHeating) {
+        this.centralHeating = centralHeating;
+    }
+
+    public Boolean getCentralCooling() {
+        return centralCooling;
+    }
+
+    public void setCentralCooling(Boolean centralCooling) {
+        this.centralCooling = centralCooling;
+    }
+
+    public Boolean getGarage() {
+        return garage;
+    }
+
+    public void setGarage(Boolean garage) {
+        this.garage = garage;
+    }
+
+    public int getSquareFootage() {
+        return squareFootage;
+    }
+
+    public void setSquareFootage(int squareFootage) {
+        this.squareFootage = squareFootage;
+    }
+
+//    public Date getListingDate() {
+//        return listingDate;
+//    }
+//
+//    public void setListingDate(Date listingDate) {
+//        this.listingDate = listingDate;
+//    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getSchoolArea() {
+        return schoolArea;
+    }
+
+    public void setSchoolArea(String schoolArea) {
+        this.schoolArea = schoolArea;
+    }
+//
+//    public List<Images> getImages() {
+//        return images;
+//    }
+//
+//    public void setImages(List<Images> images) {
+//        this.images = images;
+//    }
+//
+//    public List<PriceHistory> getPrices() {
+//        return prices;
+//    }
+//
+//    public void setPrices(List<PriceHistory> prices) {
+//        this.prices = prices;
+//    }
+
+//    public int getRealtorID() {
+//        return realtorID;
+//    }
+//
+//    public void setRealtorID(int realtorID) {
+//        this.realtorID = realtorID;
+//    }
+
+    //CONSTRUCTORS
+    public Properties() {
+    }
+
+    public Properties(ArrayList<Images> images, ArrayList<PriceHistory> prices) {
+        this.images = images;
+        this.prices = prices;
+    }
+
 }
