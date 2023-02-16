715b0a7ba9111f3b59386f0164b42842545cdad6
Saran <saran.balu@outlook.com>
2023-02-15 18:39:11 -0600

    navbar and footer component added

--- src/main/ui/src/app/app.module.ts
+++ src/main/ui/src/app/app.module.ts
@@ -1,17 +1,24 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { ListingDetailsComponent } from './listing-details/listing-details.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListingDetailsComponent
+    ListingDetailsComponent,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule, HttpClientModule,  AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
