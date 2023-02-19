739b8b1978c97cafe1d68709932e7cc0033c59ed
Saran <saran.balu@outlook.com>
2023-02-19 13:33:45 -0600

    Designing property listings

--- src/main/ui/src/app/app.module.ts
+++ src/main/ui/src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {AppRoutingModule} from './app-routing.module';
+
 
 import { AppComponent } from './app.component';
 import { ListingDetailsComponent } from './listing-details/listing-details.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const appRoutes: Routes = [
+  { path: 'app', component: AppComponent},
+  { path: 'home', component: HomeComponent },
+  { path: 'listings', component: ListingDetailsComponent }
+];
 
 @NgModule({
   declarations: [
@@ -18,7 +26,10 @@ import { FooterComponent } from './footer/footer.component';
     FooterComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule,  AppRoutingModule
+    BrowserModule, HttpClientModule, RouterModule.forRoot(appRoutes)
+  ],
+  exports: [
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
