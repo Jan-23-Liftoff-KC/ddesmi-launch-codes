9e96a2bbdfaa14480588d29c503ede3e26b7936b
Denise Paniagua <obeythecupcake@gmail.com>
2023-02-23 12:53:24 -0600

    updated userRepository

--- src/main/java/com/ddesmi/dywaboh/models/data/UserRepository.java
+++ src/main/java/com/ddesmi/dywaboh/models/data/UserRepository.java
@@ -1,5 +1,8 @@
 package com.ddesmi.dywaboh.models.data;
 
+import com.ddesmi.dywaboh.models.User;
+import org.springframework.data.repository.CrudRepository;
+
 public interface UserRepository extends CrudRepository<User, Integer> {
 
     User findByUsername(String username);
