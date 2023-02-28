f58d57bf91bd17ebe873e7bd939c07122c72d20f
Saran <saran.balu@outlook.com>
2023-02-28 09:35:59 -0600

    search function code done

--- src/main/ui/src/app/search/search.component.ts
+++ src/main/ui/src/app/search/search.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchModel } from './search';
+import { environment } from '../../environments/environment';
+import { ListingService } from '../listing/listing.service';
+import { Listing } from '../listing/listing';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-search',
@@ -7,14 +12,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  public searchObject: SearchModel;
+  private listings: Listing[];
+  private results: Listing[];
+  private javaServerUrl = environment.devServerUrl;
 
+  constructor( private listingService: ListingService) { 
+    this.listings = [];
+    this.searchObject = {
+      minbeds : 0,
+      minprice : 0,
+      maxbeds : 0,
+      maxprice : 0,
+      streetaddress : "",
+      forsale : ""
+    };
+  }
+  
   ngOnInit(): void {
+    this.listingService.getListings().subscribe(
+      (response: Listing[]) => {
+        this.listings = response;
+        console.log(this.listings);
+      },
+      (error: HttpErrorResponse) => {
+        alert("Could not fetch listings");
+      }
+    )
   }
 
   search()
   {
-    
+    this.results = [];
+
+    for(var i=0; i<this.listings.length;i++)
+    {
+      var item = this.listings[i];
+      if(item.bedrooms >= this.searchObject.minbeds && item.bedrooms <= this.searchObject.maxbeds
+        && item.address.includes(this.searchObject.streetaddress)
+        && item.price >= this.searchObject.minprice && item.price <= this.searchObject.maxprice)
+        {
+          this.results.push(item);
+        }
+    }
+    console.log(this.searchObject);
   }
 
 }
